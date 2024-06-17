8b58c6260c661985c90c50b483d2e7a11c5357d5
Beck Kurbonov <beckkurbonov@gmail.com>
2024-06-17 16:50:54 -0400

    Ticket-???: Initial commit for the fe.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NotificationForm from './components/NotificationForm';
+
+function App() {
+  return (
+    <div className="App">
+      <NotificationForm />
+    </div>
+  );
+}
+
+export default App;
